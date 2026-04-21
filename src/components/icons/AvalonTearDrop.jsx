ab5c3491acdac87de086c926b72a40f90d1a50dc
Joseph Litton <joseph@avalonvitality.co>
2026-04-21 01:51:27 +0000

    File changes

--- src/components/icons/AvalonTearDrop.jsx
+++ src/components/icons/AvalonTearDrop.jsx
@@ -1,17 +1,17 @@
 export default function AvalonTearDrop({ className = "w-5 h-5" }) {
   return (
-    <svg viewBox="0 0 100 120" className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      {/* Outer teardrop shape */}
-      <path d="M50 10C35 30 20 50 20 70C20 95 32 110 50 110C68 110 80 95 80 70C80 50 65 30 50 10Z" />
+    <svg viewBox="0 0 100 140" className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      {/* Main teardrop shape */}
+      <path d="M50 8C30 35 15 55 15 75C15 105 30 130 50 130C70 130 85 105 85 75C85 55 70 35 50 8Z" />
       
-      {/* Left triangle (A shape) */}
-      <path d="M35 50L45 30L50 45Z" fill="white" />
+      {/* Left white triangle (A) */}
+      <path d="M32 65L42 35L50 65Z" fill="white" />
       
-      {/* Right triangle (V shape) */}
-      <path d="M55 45L60 30L70 50Z" fill="white" />
+      {/* Right white triangle (V) */}
+      <path d="M50 65L58 35L68 65Z" fill="white" />
       
-      {/* Center diamond/slash */}
-      <path d="M42 40L50 50L42 80M58 40L50 50L58 80" strokeWidth="3" stroke="white" fill="none" strokeLinecap="round" />
+      {/* Diagonal slash through middle */}
+      <path d="M40 50L60 85" strokeWidth="4" stroke="white" fill="none" strokeLinecap="round" />
     </svg>
   );
 }
