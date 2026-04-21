a095aee007101b926c619564d194e247905de9d0
Joseph Litton <joseph@avalonvitality.co>
2026-04-21 01:50:52 +0000

    File changes

--- src/components/icons/AvalonTearDrop.jsx
+++ src/components/icons/AvalonTearDrop.jsx
@@ -1,7 +1,17 @@
 export default function AvalonTearDrop({ className = "w-5 h-5" }) {
   return (
-    <svg viewBox="0 0 100 100" className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path d="M50 10C50 10 30 35 30 50C30 65 38.5 75 50 75C61.5 75 70 65 70 50C70 35 50 10 50 10Z M40 50C40 45 44 40 50 40C56 40 60 45 60 50C60 58 56 65 50 65C44 65 40 58 40 50Z" />
+    <svg viewBox="0 0 100 120" className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      {/* Outer teardrop shape */}
+      <path d="M50 10C35 30 20 50 20 70C20 95 32 110 50 110C68 110 80 95 80 70C80 50 65 30 50 10Z" />
+      
+      {/* Left triangle (A shape) */}
+      <path d="M35 50L45 30L50 45Z" fill="white" />
+      
+      {/* Right triangle (V shape) */}
+      <path d="M55 45L60 30L70 50Z" fill="white" />
+      
+      {/* Center diamond/slash */}
+      <path d="M42 40L50 50L42 80M58 40L50 50L58 80" strokeWidth="3" stroke="white" fill="none" strokeLinecap="round" />
     </svg>
   );
 }
