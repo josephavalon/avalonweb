171193dd19fdf8ffc81ed0ea673c607f2dcb4053
BIG PAPA <joseph@avalonvitality.co>
2026-04-24 11:56:11 -0700

    Replace CannabisLeaf with 7-leaflet line-art outline

--- src/components/icons/CannabisLeaf.jsx
+++ src/components/icons/CannabisLeaf.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
-export default function CannabisLeaf({ className }) {
+export default function CannabisLeaf({ className, strokeWidth = 1.25 }) {
   return (
     <svg
       className={className}
       viewBox="0 0 24 24"
-      fill="currentColor"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M12 2C12 2 7 5.5 7 9.5c0 1.2.4 2.3 1 3.1C6.5 11.8 4.5 11 3 11c0 0 1.5 3.5 5 4.5-.5.8-.8 1.6-.9 2.5H10v4h4v-4h2.9c-.1-.9-.4-1.7-.9-2.5 3.5-1 5-4.5 5-4.5-1.5 0-3.5.8-5 1.6.6-.8 1-1.9 1-3.1C17 5.5 12 2 12 2z" />
+      <g transform="translate(12 14)">
+        {/* Center (tallest) */}
+        <path d="M0 0 Q -2.1 -5.5 0 -11.5 Q 2.1 -5.5 0 0 Z" />
+        {/* Upper pair */}
+        <g transform="rotate(-38)"><path d="M0 0 Q -1.9 -5 0 -10.3 Q 1.9 -5 0 0 Z" /></g>
+        <g transform="rotate(38)"><path d="M0 0 Q -1.9 -5 0 -10.3 Q 1.9 -5 0 0 Z" /></g>
+        {/* Middle pair */}
+        <g transform="rotate(-76)"><path d="M0 0 Q -1.8 -4.5 0 -9.2 Q 1.8 -4.5 0 0 Z" /></g>
+        <g transform="rotate(76)"><path d="M0 0 Q -1.8 -4.5 0 -9.2 Q 1.8 -4.5 0 0 Z" /></g>
+        {/* Lower pair */}
+        <g transform="rotate(-115)"><path d="M0 0 Q -1.6 -4 0 -8 Q 1.6 -4 0 0 Z" /></g>
+        <g transform="rotate(115)"><path d="M0 0 Q -1.6 -4 0 -8 Q 1.6 -4 0 0 Z" /></g>
+      </g>
+      {/* Stem */}
+      <line x1="12" y1="14" x2="12" y2="21.5" />
     </svg>
   );
 }
