726ae718dcdf597c15d80f1d68839e357cbca8e3
Joseph Litton <josephmbp@Josephs-MacBook-Pro.local>
2026-06-04 19:30:38 -0700

    Use versioned Avalon background asset

--- src/components/AvalonStaticBackdrop.jsx
+++ src/components/AvalonStaticBackdrop.jsx
@@ -16,8 +16,8 @@ export default function AvalonStaticBackdrop() {
   return (
     <div className="pointer-events-none fixed inset-0 z-0 h-[100lvh] min-h-screen overflow-hidden bg-black" aria-hidden="true">
       <img
-        src="/images/avalon-static-back.jpg"
-        srcSet="/images/avalon-static-back-512.jpg 512w, /images/avalon-static-back-1024.jpg 1024w, /images/avalon-static-back.jpg 1536w"
+        src="/images/avalon-static-back-20260604.jpg"
+        srcSet="/images/avalon-static-back-20260604-512.jpg 512w, /images/avalon-static-back-20260604-1024.jpg 1024w, /images/avalon-static-back-20260604.jpg 1536w"
         sizes="100vw"
         alt=""
         className="absolute inset-0 h-full w-full object-cover opacity-58 blur-[1.5px] saturate-[0.72] brightness-[0.68] [object-position:86%_52%] md:[object-position:74%_52%]"
